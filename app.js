547e589143f46c6e059193743749db24957cf110
Xinhe <xinhe998@gmail.com>
2018-12-05 19:29:46 +0800

    socket chat room

--- app.js
+++ app.js
@@ -45,18 +45,75 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/apidoc', express.static('apidoc'));
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/socket.html');
+
+
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/chat/socket.html');
+// });
+app.get('/send.png', function (req, res) {
+  res.sendFile(__dirname + '/chat/send.png');
+});
+app.get('/male.png', function (req, res) {
+  res.sendFile(__dirname + '/chat/male.png');
+});
+app.get('/female.png', function (req, res) {
+  res.sendFile(__dirname + '/chat/female.png');
 });
 
 var socketApi = require('./config/socketApi');
 var io = socketApi.io;
 
-io.on('connection', function(socket){
-  socket.on('client_message', function(msg){
-    console.log(msg);
-    socket.emit('message', msg);
+var usocket = {}, user = []; //for new user
+var typing_user = []; //for typing user list
+io.on('connection', function (socket) {
+  socket.join('chat_room');
+  io.in('chat_room').clients((error, clients) => {
+    if (error) throw error;
+    // Returns an array of client IDs like ["Anw2LatarvGVVXEIAAAD"]
   });
+  socket.on('client_message', function (msg) {
+    socket.to('chat_room').emit('message', msg);
+  });
+  socket.on('new user', (username, gender) => {
+    if (!(username in usocket)) {
+      socket.username = username;
+      socket.gender = gender;
+      usocket[username] = socket;
+      var new_user = {}
+      new_user.username = username;
+      new_user.gender = gender;
+      user.push(new_user);
+      socket.to('chat_room').emit("save_user_to_vue", user)
+      console.log(user);
+    }
+  })
+  socket.on('typing', (username, msg) => {
+    if (!(typing_user.includes(username))) {
+      // socket.username = username;
+      // typing_socket[username] = socket;
+      typing_user.push(username);
+      socket.to('chat_room').emit("save_typing_user_to_vue", typing_user)
+      console.log("typing=>", typing_user);
+    } else {
+      if (!msg) {
+        typing_user.splice(typing_user.indexOf(username), 1);
+        socket.to('chat_room').emit("save_typing_user_to_vue", typing_user)
+      }
+    }
+  })
+  socket.on('disconnect', function () {
+    //移除
+    if (socket.username in usocket) {
+      delete (usocket[socket.username]);
+      user.splice(user.indexOf(socket.username), 1);
+      socket.to('chat_room').emit("save_user_to_vue", user)
+    }
+    if (typing_user.includes(socket.username)) {
+      typing_user.splice(typing_user.indexOf(socket.username), 1);
+      socket.to('chat_room').emit("save_typing_user_to_vue", typing_user)
+    }
+    console.log(user);
+  })
 });
 
 // db state
